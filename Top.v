c16a1953d1c5a38994afa1fc62244d8e463adab7
Made <49566185+made-o@users.noreply.github.com>
2021-09-15 20:03:07 -0300

    Declaración de variables e instanciación
    Se instancia el módulo de la ALU & Se declaran variables de entrada, salida y parámetros

--- Top.v
+++ Top.v
@@ -1,26 +1,37 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.09.2021 19:37:23
-// Design Name: 
-// Module Name: Top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+//`timescale 1ns / 1ps
 
-
-module Top(
-
+module Top 
+#(  // Parámetros:
+    parameter NBITS  = 8, // bits de entrada (switches)
+    parameter COD_OP = 6
+)
+(   // Entradas:
+    input wire              clk,      // Clock
+    input wire [2,0]        pulsador, // Son los switches que eligen si se ingresan datos o una operacion
+    input wire [NBITS-1:0]  entrada,  //Bus de entrada que muestra los operandos o el codigo de operacion
+    
+    // Salidas:
+    output wire [NBITS-1:0] ALU_Out //salida de resultado
+);
+    
+    // Variables internas:
+    reg [NBITS -1:0] inA;    // Registro de entrada que almacena el operando A
+    reg [NBITS -1:0] inB;    // Registro de entrada que almacena el operando B
+    reg [COD_OP-1:0] cod_op; // Registro que almacena el codigo de la operacion
+    
+    
+    // Cosas que faltan:
+    
+    
+    
+    
+    
+    // Instancio al módulo de la ALU:
+    alu alu_0 ( 
+        .operando_A(inA),
+        .operando_B(inB),
+        .cod_operacion(cod_op),
+        .ALU_Result(ALU_Out) 
     );
+    
 endmodule
