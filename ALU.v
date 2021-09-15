8a04acd1d6a5f044c78378b8b1febe6ba82579c7
Made <49566185+made-o@users.noreply.github.com>
2021-09-15 19:48:54 -0300

    some changes in the ALU module
    operaciones: suma, resta, AND lógico, OR lógico y XOR lógico

--- ALU.v
+++ ALU.v
@@ -1,26 +1,42 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.09.2021 20:22:56
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+//`timescale 1ns / 1ps
 
+//Definicion de Macros para manipular mejor las operaciones:
+`define ADD 6'b100000   //Salida-> A + B
+`define	SUB	6'b100010	//Salida-> A - B
+`define	AND	6'b100100	//Salida-> A and B
+`define	OR	6'b100101	//Salida-> A or B
+`define	XOR	6'b100110	//Salida-> A nor B
+`define	SRA	6'b000011	//Salida-> A,B >>> 
+`define	SRL	6'b000010	//Salida-> A,B >>
+`define	NOR	6'b100111 	//Salida-> A nor B
 
-module ALU(
-
+module modulo_combinacional_alu
+#( // Parámetros:
+     parameter   NBITS  = 8
+     parameter   COD_OP = 6
+)
+ (
+     // Entradas:
+     input wire signed [NBITS-1:0]  operando_A,    // registro A
+     input wire signed [NBITS-1:0]  operando_B,    // registro B
+     input wire signed [COD_OP-1:0] cod_operacion, // la operación a realizar
+     
+     // Salidas:
+     output reg signed [NBITS-1:0]  ALU_Result // ALU Output
+        
     );
-endmodule
+    
+    always @(*)
+	begin : operaciones
+        case(cod_operacion)
+            `ADD : ALU_Result = operando_A + operando_B;
+			`SUB : ALU_Result = operando_A - operando_B;
+			`AND : ALU_Result = operando_A & operando_B;
+			`OR  : ALU_Result = operando_A | operando_B;
+			`XOR : ALU_Result = operando_A ^ operando_B;
+            
+            
+        endcase
+	end
+
+endmodule//alu_module
