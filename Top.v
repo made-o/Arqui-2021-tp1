8836321966dcb3e0803254e1581eac1c7b121658
Made <49566185+made-o@users.noreply.github.com>
2021-09-16 10:43:40 -0300

    Desarrollo del top_module
    Se agregó el bloque que guarda los operandos y la operación en los registros auxiliares.

--- Top.v
+++ Top.v
@@ -6,12 +6,13 @@ module Top
     parameter COD_OP = 6
 )
 (   // Entradas:
-    input wire              clk,      // Clock
-    input wire [2:0]        pulsador, // Son los switches que eligen si se ingresan datos o una operacion
-    input wire [NBITS-1:0]  entrada,  //Bus de entrada que muestra los operandos o el codigo de operacion
+    input wire              clk,       // Clock
+    input wire              btn_Reset, // Botón de reset de entradas y cógido de operación
+    input wire [2:0]        pulsador,  // Son los switches que eligen si se ingresan datos o una operacion
+    input wire [NBITS-1:0]  entrada,   // Bus de entrada que muestra los operandos o el codigo de operacion
     
     // Salidas:
-    output wire [NBITS-1:0] ALU_Out //salida de resultado
+    output wire [NBITS-1:0] ALU_Out   // Salida del resultado
 );
     
     // Variables internas:
@@ -21,9 +22,24 @@ module Top
     
     
     // Cosas que faltan:
-    
-    
-    
+        // Bloque que guarda los operandos y la operación en los registros auxiliares.
+        // Lo hace sincronicamente
+    always  @(posedge clk)
+    begin : seleccion
+        case(pulsador)
+            3'b001 : op_A   = entrada;
+            3'b010 : op_B   = entrada;
+            3'b100 : cod_op = entrada[5:0];  //El codigo de operacion solo ocupa los 6 bits menos significativos del bus de entrada
+            default : ;
+        endcase
+        
+        if(btn_Reset) // Reseteo de entradas y código de operación:
+        begin
+            inA    <= {NBITS {1'b0}};
+            inB    <= {NBITS {1'b0}};
+            cod_op <= {COD_OP{1'b0}};
+        end
+    end
     
     
     // Instancio al módulo de la ALU:
@@ -34,4 +50,4 @@ module Top
         .ALU_Result(ALU_Out) 
     );
     
-endmodule
+endmodule // Top_module
